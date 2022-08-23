f366f26950383e2e4d0a6670868c76c7aacb1657
Евгений <Khodov1992@mail.ru>
2022-08-23 23:23:57 +0500

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -15,10 +15,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("Сергей", "Петров", "Сергей@mail.ru"));
+      userService.add(new User("Иван", "Иванов", "Иван@mail.ru"));
+      userService.add(new User("Максим", "Сидоров", "Максим@mail.ru"));
+      userService.add(new User("Дмитрий", "Богданов", "Дмитрий@mail.ru"));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
