d64627c49ac9a029586c2babb264d1d3a57f7219
Евгений <Khodov1992@mail.ru>
2022-08-24 09:31:01 +0500

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,32 +1,49 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.SQLException;
-import java.util.List;
+import javax.persistence.NoResultException;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("Сергей", "Петров", "Сергей@mail.ru"));
-      userService.add(new User("Иван", "Иванов", "Иван@mail.ru"));
-      userService.add(new User("Максим", "Сидоров", "Максим@mail.ru"));
-      userService.add(new User("Дмитрий", "Богданов", "Дмитрий@mail.ru"));
+      User user1 = new User("Sergey", "Petrov", "Pettrov@hogwarts.com");
+      User user2 = new User("Anna", "Sidorova", "Sidorova@hogwarts.com");
+      User user3 = new User("Roman", "Kovolev", "Kowolev@hogwarts.com");
+      User user4 = new User("Liza", "Ivanova", "Ivanova@hogwarts.com");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car car1 = new Car("BMW", 5);
+      Car car2 = new Car("AUDI", 6);
+      Car car3 = new Car("OPEL", 72);
+      Car car4 = new Car("FORD", 2);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println("1. Пользователь с машиной");
+         System.out.println(user + " " + user.getCar());
+         System.out.println(" ");
+      }
+
+      System.out.println("2. пользователь, владеющей машиной по ее модели и серии");
+      System.out.println(userService.getUserByCar("BMW", 5));
+      System.out.println(" ");
+
+      try {
+         System.out.println("3. Такого пользователя не найдено");
+         User notFoundUser = userService.getUserByCar("WV", 9);
+      } catch (NoResultException e) {
+
       }
 
       context.close();
